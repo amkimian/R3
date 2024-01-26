236e07e451021d47ac2398383e0d5765d9295f8e
Alan Moore <ukmoore@gmail.com>
2024-01-26 13:30:00 -0800

    Updated test to assert

--- src/test/java/com/staberinde/sscript/BasicTest.java
+++ src/test/java/com/staberinde/sscript/BasicTest.java
@@ -3,9 +3,12 @@ package com.staberinde.sscript;
 import com.staberinde.sscript.main.MainParser;
 import com.staberinde.sscript.model.BlockProgram;
 import com.staberinde.sscript.model.BlockProgramContext;
+import com.staberinde.sscript.value.BlockValue;
 import org.junit.Test;
 
 import java.io.IOException;
+
+import static org.junit.Assert.assertTrue;
 
 public class BasicTest {
 
@@ -14,6 +17,7 @@ public class BasicTest {
         // Tests parsing and executing SS Script
         BlockProgram program = MainParser.parse("x=1;y=2*x;z=3*y;z;", null);
         BlockProgramContext ctx = new BlockProgramContext();
-        System.out.println(program.run(ctx));
+        BlockValue res = program.run(ctx);
+        assertTrue(res.asInteger() == 6);
     }
 }
